effa30c3fc4883d09bdc2dc188af92e50a992778
boertel <ben@comediadesign.com>
2021-11-16 10:16:18 -0800

    add tests

--- src/duration.ts
+++ src/duration.ts
@@ -1,13 +1,16 @@
-export function duration(seconds) {
-  let output = [];
-  let remainder = seconds;
-  const durations = [
+export function duration(inputInSeconds?: number): Duration {
+  let output: number[] = [];
+  if (inputInSeconds === undefined) {
+    return new Duration();
+  }
+  let remainder: number = inputInSeconds;
+  const durations: number[] = [
     // Number of seconds in
     24 * 60 * 60, // a day
     60 * 60, // a hour
     1 * 60, // a minute
   ];
-  durations.forEach((divisor, index) => {
+  durations.forEach((divisor: number, index: number) => {
     const quotient: number = Math.abs(parseInt(`${remainder / divisor}`, 10));
     remainder = Math.abs(remainder % divisor);
     output.push(Math.floor(quotient));
@@ -15,10 +18,152 @@ export function duration(seconds) {
       output.push(Math.floor(remainder));
     }
   });
-  return output.reverse();
+  return new Duration(output);
 }
 
-function pad(value) {
+class Duration {
+  private _seconds: number = 0;
+  private _minutes: number = 0;
+  private _hours: number = 0;
+  private _days: number = 0;
+
+  constructor(
+    args?:
+      | number[]
+      | { seconds: number; minutes: number; hours: number; days: number }
+  ) {
+    if (Array.isArray(args)) {
+      this._seconds = args[3];
+      this._minutes = args[2];
+      this._hours = args[1];
+      this._days = args[0];
+    } else if (typeof args === "object") {
+      this._seconds = args.seconds;
+      this._minutes = args.minutes;
+      this._hours = args.hours;
+      this._days = args.days;
+    }
+  }
+
+  seconds(inputInSeconds: number): Duration {
+    this._seconds = inputInSeconds;
+    return this;
+  }
+
+  minutes(inputInMinutes: number): Duration {
+    this._minutes = inputInMinutes;
+    return this;
+  }
+
+  hours(inputInHours: number): Duration {
+    this._hours = inputInHours;
+    return this;
+  }
+
+  days(inputInDays: number): Duration {
+    this._days = inputInDays;
+    return this;
+  }
+
+  toArray(): number[] {
+    return [this._seconds, this._minutes, this._hours, this._days];
+  }
+
+  get(str: SingleFormat): number {
+    const caseInsensitive = str.toLowerCase();
+    switch (caseInsensitive) {
+      case "d":
+        return this._days;
+      case "h":
+        return this._hours;
+      case "m":
+        return this._minutes;
+      case "s":
+        return this._seconds;
+      default:
+        throw new Error("Invalid format");
+    }
+  }
+
+  private _sentence(str: string[]): string {
+    const options: FormatOptions = {
+      ignoreZero: true,
+    };
+    let parts: string[] = str
+      .filter((f: string): boolean => {
+        if (options.ignoreZero) {
+          return parseInt(this.format(f), 10) !== 0;
+        }
+        return true;
+      })
+      .map((f: string): string => this.format(f));
+
+    const last: string | undefined = parts.pop();
+    let output = [parts.join(", ")];
+    if (last) {
+      if (parts.length === 0) {
+        return last;
+      } else {
+        output.push(last);
+      }
+    }
+    return output.join(" and ");
+  }
+
+  private _format(str: string): string {
+    const matches: FormatMatches = {
+      d: String(this._days),
+      dd: pad(this._days),
+      D: "d",
+      DD: pluralize(this._days, "day", "days"),
+      h: String(this._hours),
+      hh: pad(this._hours),
+      H: "h",
+      HH: pluralize(this._hours, "hour", "hours"),
+      m: String(this._minutes),
+      mm: pad(this._minutes),
+      M: "m",
+      MM: pluralize(this._minutes, "minute", "minutes"),
+      s: String(this._seconds),
+      ss: pad(this._seconds),
+      S: "s",
+      SS: pluralize(this._seconds, "second", "seconds"),
+    };
+
+    return str.replace(REGEX_FORMAT, (match, $1): string => {
+      return $1 || String(matches[match]);
+    });
+  }
+
+  format(str: string | string[]): string {
+    if (Array.isArray(str)) {
+      return this._sentence(str);
+    } else {
+      return this._format(str);
+    }
+  }
+}
+
+interface FormatOptions {
+  ignoreZero: boolean;
+}
+
+enum SingleFormat {
+  d = "d",
+  h = "h",
+  m = "m",
+  s = "s",
+}
+
+interface FormatMatches {
+  [key: string]: string;
+}
+
+// detect [anything] and then all matches possible
+const REGEX_FORMAT =
+  /\[([^\]]+)]|D{1,2}|d{1,2}|H{1,2}|h{1,2}|M{1,2}|m{1,2}|S{1,2}|s{1,2}/g;
+
+function pad(value: number): string {
   let s = String(value);
   while (s.length < 2) {
     s = "0" + s;
@@ -26,89 +171,9 @@ function pad(value) {
   return s;
 }
 
-const defaultFormat = (value, key) =>
-  `${value} ${value === 1 ? key : `${key}s`}`;
-const defaultSeparator = (index, length) => {
-  if (index === 0) {
-    return "";
-  }
-  if (index === length - 1) {
-    return " and ";
-  } else {
-    return ", ";
-  }
-};
-
-export function formatDuration(seconds, options = {}) {
-  options = {
-    ignoreZero: true,
-    format: defaultFormat,
-    separator: defaultSeparator,
-    intervals: ["day", "hour", "minute", "second"],
-    ...options,
-  };
-
-  const { format, ignoreZero, separator, intervals } = options;
-
-  const parts = duration(seconds).reverse();
-
-  const output = parts
-    .map((part, index) => {
-      return format(part, intervals[index]);
-    })
-    .filter((part, index) => {
-      let isNotZero: boolean = ignoreZero && parts[index] !== 0;
-      return isNotZero && part.length > 0;
-    });
-
-  return output
-    .reduce(
-      (previous, current, index) =>
-        `${previous}${separator(index, output.length)}${current}`,
-      ""
-    )
-    .trim();
-}
-
-export function formatDurationShort(seconds, options = {}) {
-  options = {
-    intervals: ["d", "h", "m", "s"],
-    format: (value, key) =>
-      `${["d", "h"].includes(key) ? value : pad(value)}${key}`,
-    separator: () => " ",
-    ...options,
-  };
-  return formatDuration(seconds, options);
-}
-
-export function pluralize(count, singular, plural, returnCount) {
-  const prefix = returnCount ? count + " " : "";
+function pluralize(count: number, singular: string, plural: string): string {
   if (count === 1) {
-    return prefix + singular;
-  } else {
-    if (plural) {
-      return prefix + plural;
-    } else {
-      const lastCharacter = singular.length - 1;
-      if (singular[lastCharacter] === "y") {
-        return `${prefix}${singular.substr(0, lastCharacter)}ies`;
-      } else {
-        return `${prefix}${singular}s`;
-      }
-    }
+    return singular;
   }
-}
-
-export function formatMinutes(minutes) {
-  let duration = minutes;
-  let singular = "minute";
-  let plural = "minutes";
-
-  if (duration > 60) {
-    duration = duration / 60;
-    singular = "hour";
-    plural = "hours";
-  }
-
-  return pluralize(parseInt(duration, 10), singular, plural, true);
+  return plural;
 }
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,111 @@
-describe("Array", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+import "mocha";
+import { assert } from "chai";
+import { duration } from "../src/duration";
+
+const tests = [
+  { seconds: 0, expected: [0, 0, 0, 0] },
+  { seconds: 59, expected: [59, 0, 0, 0] },
+  { seconds: 60, expected: [0, 1, 0, 0] },
+  { seconds: 65, expected: [5, 1, 0, 0] },
+  { seconds: 120, expected: [0, 2, 0, 0] },
+  { seconds: 60 * 60, expected: [0, 0, 1, 0], message: "1 hour" },
+  { seconds: 24 * 60 * 60, expected: [0, 0, 0, 1], message: "1 day" },
+  { seconds: 5 * 24 * 60 * 60, expected: [0, 0, 0, 5], message: "5 days" },
+  {
+    seconds: 24 * 60 * 60 + 60 * 60 * 14 + 60 * 3 + 12,
+    expected: [12, 3, 14, 1],
+    message: "1 day, 14 hours, 3 minutes and 12 seconds",
+  },
+  {
+    seconds: 370 * 24 * 60 * 60,
+    expected: [0, 0, 0, 370],
+    message: "370 days",
+  },
+];
+
+describe("duration function", () => {
+  tests.forEach(({ seconds, expected, message }) => {
+    it(`${seconds} seconds ${message ? `= ${message}` : ""}`, () => {
+      assert.sameOrderedMembers(duration(seconds).toArray(), expected, message);
     });
+  });
+});
+
+describe("create duration with chaining", () => {
+  it("create seconds", () => {
+    assert.sameOrderedMembers(duration().seconds(20).toArray(), [20, 0, 0, 0]);
+  });
+
+  it("create seconds, minutes and hours", () => {
+    assert.sameOrderedMembers(
+      duration().seconds(20).minutes(23).hours(40).toArray(),
+      [20, 23, 40, 0]
+    );
+  });
+});
+
+describe("duration format", () => {
+  it("seconds", () => {
+    assert.equal(duration(1).format("s"), "1", "single digit");
+    assert.equal(duration(1).format("ss"), "01", "double digits");
+    assert.equal(
+      duration(1).format("s SS"),
+      "1 second",
+      "single digit with long label"
+    );
+    assert.equal(
+      duration(1).format("ss SS"),
+      "01 second",
+      "double digits with long label"
+    );
+    assert.equal(
+      duration(1).format("sS"),
+      "1s",
+      "single digit with short label"
+    );
+  });
+
+  it("seconds and minutes", () => {
+    const d = duration().seconds(43).minutes(12);
+    assert.equal(d.format("mm:ss"), "12:43", "double digits");
+    assert.equal(
+      d.format("mm MM and ss SS"),
+      "12 minutes [and] 43 seconds",
+      "double digits with long labels"
+    );
+  });
+
+  it("with an array to generate a sentence", () => {
+    const d = duration().seconds(23).minutes(21).hours(2);
+    assert.equal(
+      d.format(["d DD", "h HH", "m MM", "s SS"]),
+      "2 hours, 21 minutes and 23 seconds"
+    );
+  });
+
+  it("with an array to generate a sentence with none 0 values", () => {
+    const d = duration().seconds(23).minutes(21).hours(2).days(18);
+    assert.equal(
+      d.format(["d DD", "h HH", "m MM", "s SS"]),
+      "18 days, 2 hours, 21 minutes and 23 seconds"
+    );
+  });
+
+  it("with an array to generate a sentence with a bunch of 0 values", () => {
+    const d = duration().seconds(23).minutes(0).hours(0).days(18);
+    assert.equal(
+      d.format(["d DD", "h HH", "m MM", "s SS"]),
+      "18 days and 23 seconds"
+    );
+  });
+
+  it("with an array to generate a sentence with one value", () => {
+    const d = duration().minutes(12);
+    assert.equal(d.format(["d DD", "h HH", "m MM", "s SS"]), "12 minutes");
+  });
+
+  it("with an array to generate a sentence with format", () => {
+    const d = duration().minutes(12).hours(20);
+    assert.equal(d.format(["h HH"]), "20 hours");
   });
 });
