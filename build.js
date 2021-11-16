d5975778ba23f920cd7963e8d9962c7b042e0675
boertel <ben@comediadesign.com>
2021-11-16 12:23:57 -0800

    add .d.ts

--- build.js
+++ build.js
@@ -1,7 +1,35 @@
 #!/usr/bin/env node
-
-const { build } = require("estrella");
 const pkg = require("./package.json");
+const { build, ts, tsconfig, dirname, glob, log } = require("estrella");
+
+function generateTypeDefs(tsconfig, entryfiles, outdir) {
+  const filenames = Array.from(
+    new Set(
+      (Array.isArray(entryfiles) ? entryfiles : [entryfiles]).concat(
+        tsconfig.include || []
+      )
+    )
+  ).filter((v) => v);
+  log.info("Generating type declaration files for", filenames.join(", "));
+  const compilerOptions = {
+    ...tsconfig.compilerOptions,
+    moduleResolution: undefined,
+    declaration: true,
+    outDir: outdir,
+  };
+  const program = ts.ts.createProgram(filenames, compilerOptions);
+  const targetSourceFile = undefined;
+  const writeFile = undefined;
+  const cancellationToken = undefined;
+  const emitOnlyDtsFiles = true;
+  program.emit(
+    targetSourceFile,
+    writeFile,
+    cancellationToken,
+    emitOnlyDtsFiles
+  );
+  log.info("Wrote", glob(outdir + "/*.d.ts").join(", "));
+}
 
 build({
   bundle: true,
@@ -17,4 +45,8 @@ build({
   format: "esm",
   entry: "src/duration.ts",
   outfile: pkg.module,
+  onEnd(config) {
+    const dtsFilesOutdir = dirname(config.outfile);
+    generateTypeDefs(tsconfig(config), config.entry, dtsFilesOutdir);
+  },
 });
